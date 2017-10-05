890ff6700467ed89ba2cf70b0bdbd11b01cdbcf6
Théo Gallo <32473578+LightThe@users.noreply.github.com>
2017-10-05 13:07:07 -0300

    Adicionado Main
    Apenas com a estrutura básica de um menu

---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int menu(){
+    int opcao = -1;
+    printf("------------------------\n");
+    printf("SELECIONE O MODO DE USO.\n");
+    printf("------------------------\n");
+    return opcao;
+}
+int main(){
+    menu();
+    return 0;
+}
