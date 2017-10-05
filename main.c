bd5ff07416b3bc836e589f64a77674293884e376
gallo <gallotheo@icloud.com>
2017-10-05 13:33:53 -0300

    Sistema de menu principal construído.

--- main.c
+++ main.c
@@ -1,12 +1,29 @@
 #include<stdio.h>
-int menu(){
-    int opcao = -1;
-    printf("------------------------\n");
-    printf("SELECIONE O MODO DE USO.\n");
-    printf("------------------------\n");
-    return opcao;
+int menuMain(){
+  int opcao = -1;
+  printf("\t\t\t----------------------------\n");
+  printf("\t\t\t  SELECIONE O MODO DE USO.\n");
+  printf("\t\t\t----------------------------\n");
+	printf("\t1- Resolver Expressão.\n\t2- Calculadora.\nSelecione a opção inserindo seu número: \n");
+	scanf("%d", &opcao);
+  return opcao;
 }
 int main(){
-    menu();
-    return 0;
+  int opc;
+	do{
+		opc = menu();
+		switch(opc){
+			case 1:
+				printf("Opção selecionada: Resolver Expressão.\n");
+			break;
+			case 2:
+				printf("Opção selecionada: Calculadora.\n");
+			break;
+			default:
+				printf("A opção selecionada não existe!!\n");
+			break;
+		}
+	}while (opc!=1 && opc!=2);
+
+	return 0;
 }
