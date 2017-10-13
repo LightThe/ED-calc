790613e6060366f23012e7fec455ca742ce7f07c
gallo <32473578+LightThe@users.noreply.github.com>
2017-10-13 11:03:21 -0300

    rascunho de funções adicionados em pilha.h

--- pilha.h
+++ pilha.h
@@ -2,3 +2,13 @@
 #include<stdlib.h>
 typedef struct dados Data;
 typedef struct node Pilha;
+//cria e inicializa uma nova pilha
+Pilha **novaPilha();
+//empilha um item no topo
+int empilhaTopo(Pilha **stack, Data elem);
+//desempilha um item do topo
+int desempilhaTopo(Pilha **stack, Data *elem);
+//imprime todo o conteúdo da pilha
+int imprimePilha(Pilha **stack);
+//verifica se a pilha é vazia
+int pilha_vazia(Pilha **stack);
